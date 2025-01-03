e2dd0486ffcb55343532072eead4686c8a1a1ccd
Jordeilson Silva Lima <jordeilsonlima9@gmail.com>
2025-01-03 09:23:42 -0300

    Automação

---
+++ app3.py
@@ -0,0 +1,9 @@
+# Exercicio, fazer uma automação para criar um pasta.
+
+import pyautogui
+
+pyautogui.click(x=718,y=317,clicks =1, interval=1, button='right', )
+pyautogui.move(50,150,duration=1)
+pyautogui.doubleClick()
+pyautogui.moveTo(1028,83,duration=1)
+pyautogui.click()
